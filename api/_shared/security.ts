593a63034fed43c35360197af2152c4ad30f83ce
bayastley <bayastley@gmail.com>
2026-08-03 21:13:31 +0300

    Update security.ts

--- api/_shared/security.ts
+++ api/_shared/security.ts
@@ -1,5 +1,7 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
 export function requireAllowedOrigin(req: VercelRequest, res: VercelResponse): boolean {
+  void req;
+  void res;
   return true;
 }
