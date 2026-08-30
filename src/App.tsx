3b73295fcff4e233381b142168170ee7552ec96a
bayastley <bayastley@gmail.com>
2026-08-30 11:49:45 +0300

    app

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import StudioApp from "./StudioApp";
 
 // Production'da açık bırakılmaz. God Mode yalnızca /admin girişinden, oturum belleğinde açılır.
 // Kilit durumu: true = tüm kilitler açık (geliştirme/test), false = normal kilitli mod
-const isMasterSürüm = true;
+const isMasterSürüm = false;
 
 export default function App() {
   return <StudioApp isMasterSürüm={isMasterSürüm} />;
